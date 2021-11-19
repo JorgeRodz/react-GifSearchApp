877176e434989f6556d671ee09e8b5fac4692a98
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2021-11-19 15:10:29 -0600

    Update the docs folder and add fisrt testing configurations

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -14,3 +14,14 @@ const GifGridItem = ({ id, title, url }) => {
 GifGridItem.propTypes = {};
 
 export default GifGridItem;
+
+/*
+
+  1. Enzyme
+  2. Enzyme to Json
+  3. Debe de mostrar el component correctamente
+    - shallow
+    - wrapper
+    - wrapper .toMatchSnapshot()
+
+*/
